31a138356999b397ac78a1649be2ab7879634aab
Katelyn M <255952759+katem26@users.noreply.github.com>
2026-09-11 10:46:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Binary Sudoku
+Binary sudoku/takuzu game for kindles.
+
+# Usage
+Copy both the _binarySudoku/_ folder and _binarySudoku.sh_ to the documents folder.
---
+++ binarySudoku/script.js
@@ -0,0 +1,536 @@
+var sixBySixBoards =
+[
+    [
+        [-1, -1,  0, -1, -1, -1],
+        [-1, -1, -1, -1, -1, -1],
+        [ 1, -1, -1,  1,  0, -1],
+        [-1, -1, -1,  1, -1, -1],
+        [-1, -1, -1, -1,  0, -1],
+        [ 1,  1, -1,  0, -1, -1]
+    ],
+    [
+        [ 1,  0, -1, -1,  0, -1],
+        [ 1, -1, -1, -1, -1,  0],
+        [-1, -1, -1, -1,  1,  0],
+        [-1,  1,  0, -1, -1, -1],
+        [-1, -1,  0, -1, -1, -1],
+        [-1, -1, -1, -1, -1, -1]
+    ],
+    [
+        [ 1, -1, -1,  1, -1, -1],
+        [ 1, -1,  0, -1, -1,  0],
+        [-1, -1, -1, -1, -1,  0],
+        [-1, -1, -1,  1, -1, -1],
+        [-1, -1, -1, -1,  0,  0],
+        [-1, -1, -1, -1, -1, -1]
+    ],
+    [
+        [ 1, -1, -1,  0, -1, -1],
+        [ 1, -1,  1, -1,  1, -1],
+        [-1, -1,  0, -1, -1,  0],
+        [-1, -1, -1,  0, -1, -1],
+        [-1, -1, -1, -1, -1, -1],
+        [-1, -1, -1, -1, -1, -1]
+    ],
+    [
+        [-1, -1, -1,  0, -1,  0],
+        [-1, -1, -1, -1, -1, -1],
+        [-1,  0, -1, -1, -1, -1],
+        [-1, -1, -1,  1, -1, -1],
+        [-1, -1,  1,  1, -1,  0],
+        [-1, -1, -1, -1,  0, -1]
+    ],
+    [
+        [-1, -1,  1, -1, -1, -1],
+        [-1, -1,  1, -1, -1,  1],
+        [ 1, -1, -1, -1,  1, -1],
+        [-1, -1, -1,  1, -1, -1],
+        [ 1, -1,  0, -1, -1, -1],
+        [-1, -1, -1,  1, -1, -1]
+    ],
+    [
+        [-1, -1,  1, -1, -1, -1],
+        [-1, -1,  0,  0, -1, -1],
+        [ 1, -1, -1, -1,  0,  1],
+        [-1, -1, -1,  1, -1, -1],
+        [-1,  1,  1, -1, -1,  1],
+        [ 1, -1, -1, -1, -1, -1]
+    ],
+    [
+        [-1, -1,  1, -1,  1,  1],
+        [-1, -1, -1, -1, -1, -1],
+        [-1,  0, -1, -1, -1,  0],
+        [-1, -1,  1, -1, -1, -1],
+        [-1, -1, -1,  0, -1,  0],
+        [-1, -1,  1, -1, -1, -1]
+    ],
+    [
+        [-1, -1, -1, -1,  1, -1],
+        [ 1, -1, -1, -1, -1, -1],
+        [-1, -1, -1,  1,  1, -1],
+        [ 1, -1, -1, -1,  1,  0],
+        [-1, -1, -1,  0, -1, -1],
+        [-1, -1,  1, -1, -1, -1]
+    ],
+    [
+        [ 1, -1, -1, -1,  0, -1],
+        [ 1, -1, -1,  0, -1,  0],
+        [-1, -1, -1,  0, -1, -1],
+        [ 0, -1, -1, -1,  1, -1],
+        [-1, -1,  1, -1, -1, -1],
+        [-1, -1, -1, -1, -1,  1]
+    ],
+    [
+        [-1, -1,  1, -1, -1,  1],
+        [ 0, -1, -1,  0, -1, -1],
+        [ 0, -1, -1, -1, -1, -1],
+        [-1, -1, -1, -1, -1, -1],
+        [-1, -1,  0,  0, -1, -1],
+        [-1,  1, -1, -1,  0, -1]
+    ],
+    [
+        [-1, -1, -1,  1, -1,  1],
+        [ 0,  0, -1, -1, -1, -1],
+        [-1, -1,  1, -1,  0, -1],
+        [-1, -1, -1,  1, -1, -1],
+        [-1,  0, -1, -1, -1,  1],
+        [-1, -1, -1, -1, -1, -1]
+    ],
+    [
+        [-1,  0, -1, -1, -1, -1],
+        [ 1,  0, -1, -1, -1,  0],
+        [-1, -1, -1, -1,  1,  0],
+        [-1, -1, -1,  0,  0, -1],
+        [-1, -1, -1, -1, -1, -1],
+        [ 0, -1, -1,  0,  0, -1]
+    ],
+    [
+        [-1, -1, -1, -1, -1, -1],
+        [-1, -1, -1,  1, -1, -1],
+        [ 0, -1,  0, -1, -1, -1],
+        [-1,  1, -1, -1,  0, -1],
+        [ 0, -1, -1, -1, -1, -1],
+        [ 0,  1, -1, -1,  0, -1]
+    ],
+    [
+        [ 0, -1, -1,  1, -1, -1],
+        [-1, -1,  1, -1,  1, -1],
+        [-1,  0, -1,  1, -1,  0],
+        [-1, -1, -1,  1, -1,  1],
+        [-1, -1, -1, -1,  1, -1],
+        [-1, -1, -1, -1, -1,  0]
+    ],
+    [
+        [-1,  1, -1,  1, -1, -1],
+        [ 0, -1, -1, -1,  1, -1],
+        [-1, -1, -1,  0, -1, -1],
+        [-1, -1, -1,  0, -1,  0],
+        [-1,  0,  0, -1, -1, -1],
+        [-1, -1, -1, -1, -1,  1]
+    ],
+    [
+        [-1, -1, -1,  1, -1,  1],
+        [-1, -1, -1,  0, -1,  0],
+        [ 1, -1,  1, -1, -1, -1],
+        [-1, -1, -1, -1, -1, -1],
+        [-1, -1,  1,  0, -1,  1],
+        [-1, -1,  1, -1, -1, -1]
+    ],
+    [
+        [-1,  1,  1, -1, -1, -1],
+        [ 1, -1, -1, -1, -1, -1],
+        [-1,  0, -1,  1, -1,  0],
+        [-1,  0, -1,  1, -1, -1],
+        [-1, -1, -1, -1, -1,  0],
+        [-1, -1, -1, -1, -1, -1]
+    ],
+    [
+        [-1,  1, -1, -1, -1,  1],
+        [-1, -1,  1, -1,  1, -1],
+        [-1, -1, -1,  0, -1, -1],
+        [-1, -1, -1, -1, -1,  0],
+        [ 0, -1, -1, -1, -1, -1],
+        [-1,  0, -1,  0, -1,  0]
+    ],
+    [
+        [-1, -1, -1,  1, -1,  1],
+        [ 1,  1, -1, -1, -1, -1],
+        [ 1, -1, -1, -1, -1, -1],
+        [-1,  0, -1, -1,  0, -1],
+        [-1, -1, -1,  0, -1, -1],
+        [-1, -1, -1, -1,  0, -1]
+    ]
+];
+
+var boardSize=6;
+var currentBoardList = sixBySixBoards; // i'll add logic for changing the list if i add more board sizes
+
+var boardDisplay, message;
+var boardNumber, currentBoard, initialTiles;
+
+function cloneBoard(board) // make a deep copy of the nested array
+{
+    var clone = [];
+    for (var row=0; row<boardSize; row++)
+    {
+        var new_row = [];
+        for (var column=0; column<boardSize; column++)
+        {
+            new_row.push(board[row][column]);
+        }
+        clone.push(new_row);
+    }
+    return clone;
+}
+
+function getFilledTiles(board)
+{
+    var filled = [];
+    for (var row=0; row<boardSize; row++)
+    {
+        var new_row = [];
+        for (var column=0; column<boardSize; column++)
+        {
+            if (board[row][column] == -1)
+            {
+                new_row.push(false);
+            }
+            else
+            {
+                new_row.push(true);
+            }
+        }
+        filled.push(new_row);
+    }
+    return filled;
+}
+
+function drawBoard()
+{
+    var html = "";
+
+    for (var row = 0; row < boardSize; row++)
+    {
+        html += "<tr>";
+
+        for (var column = 0; column < boardSize; column++)
+        {
+            var value = currentBoard[row][column];
+
+            if (value == 0)
+            {
+                html += "<td class='zero' onclick='clickCell("+row+", "+column+")'></td>"
+            }
+            else if (value == 1)
+            {
+                html += "<td class='one' onclick='clickCell("+row+", "+column+")'></td>"
+            }
+            else
+            {
+                html += "<td onclick='clickCell("+row+", "+column+")'></td>"
+            }
+        }
+
+        html += "</tr>";
+    }
+
+    boardDisplay.innerHTML = html;
+}
+
+function clickCell(row, column)
+{
+    if (initialTiles[row][column])
+    {
+        return; // don't allow the initial tiles to be changed
+    }
+    var value = currentBoard[row][column];
+
+    if (value == -1)
+    {
+        currentBoard[row][column] = 0;
+    }
+    else if (value == 0)
+    {
+        currentBoard[row][column] = 1;
+    }
+    else
+    {
+        currentBoard[row][column] = -1;
+    }
+
+    drawBoard();
+    checkBoard();
+}
+
+function isFilledRow(row)
+{
+    for (var column=0; column<boardSize; column++)
+    {
+        if (currentBoard[row][column] == -1)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+function isFilledColumn(column)
+{
+    for (var row=0; row<boardSize; row++)
+    {
+        if (currentBoard[row][column] == -1)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+function isBoardFull()
+{
+    for (var row=0; row<boardSize; row++)
+    {
+        if (!isFilledRow(row))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+function isValidFilledRow(row)
+{
+    // check if filled
+    if (!isFilledRow(row))
+    {
+        return false;
+    }
+
+    // check if there are the same amount of each colour
+    var ones=0;
+    var zeroes=0;
+
+    for (var column=0; column < boardSize; column++)
+    {
+        if (currentBoard[row][column] == 0)
+        {
+            zeroes++;
+        }
+
+        if (currentBoard[row][column] == 1)
+        {
+            ones++;
+        }
+    }
+
+    if (ones != boardSize/2 || zeroes != boardSize/2)
+    {
+        return false;
+    }
+
+    // check for three adjacent tiles of the same colour
+    for (var i=0; i<boardSize-3; i++)
+    {
+        if (currentBoard[row][i]==currentBoard[row][i+1] && currentBoard[row][i+1]==currentBoard[row][i+2])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function isValidFilledColumn(column)
+{
+    // check if filled
+    if (!isFilledColumn(column))
+    {
+        return false;
+    }
+
+    // check if there are the same amount of each colour
+    var ones=0;
+    var zeroes=0;
+
+    for (var row=0; row < boardSize; row++)
+    {
+        if (currentBoard[row][column] == 0)
+        {
+            zeroes++;
+        }
+
+        if (currentBoard[row][column] == 1)
+        {
+            ones++;
+        }
+    }
+
+    if (ones != boardSize/2 || zeroes != boardSize/2)
+    {
+        return false;
+    }
+
+    // check for three adjacent tiles of the same colour
+    for (var i=0; i<boardSize-3; i++)
+    {
+        if (currentBoard[i][column]==currentBoard[i+1][column] && currentBoard[i+1][column]==currentBoard[i+2][column])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function rowsMatch(row1, row2)
+{
+    for (var column=0; column<boardSize; column++)
+    {
+        if (currentBoard[row1][column] != currentBoard[row2][column])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+function columnsMatch(column1, column2)
+{
+    for (var row=0; row<boardSize; row++)
+    {
+        if (currentBoard[row][column1] != currentBoard[row][column2])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+function anyDuplicateRows()
+{
+    for (var i=0; i<boardSize; i++)
+    {
+        for (var j=i+1; j<boardSize; j++)
+        {
+            if (rowsMatch(i, j))
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function anyDuplicateColumns()
+{
+    for (var i=0; i<boardSize; i++)
+    {
+        for (var j=i+1; j<boardSize; j++)
+        {
+            if (columnsMatch(i, j))
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+
+function checkBoard()
+{
+    message.innerHTML = ""; // reset message
+
+    // check rows
+    for (var row=0; row < boardSize; row++)
+    {
+        if (isFilledRow(row))
+        {
+            if (!isValidFilledRow(row))
+            {
+                message.innerHTML = "There's something wrong with a row...";
+                return;
+            }
+        }
+    }
+
+    // check columns
+    for (var column=0; column < boardSize; column++)
+    {
+        if (isFilledColumn(column))
+        {
+            if (!isValidFilledColumn(column))
+            {
+                message.innerHTML = "There's something wrong with a column...";
+                return;
+            }
+        }
+    }
+
+    if (isBoardFull())
+    {
+        // check for duplicates
+        if (anyDuplicateRows())
+        {
+            message.innerHTML = "Two rows are the same...";
+        }
+        else if (anyDuplicateColumns())
+        {
+            message.innerHTML = "Two columns are the same...";
+        }
+        else
+        {
+            message.innerHTML = "Congratulations! You win :-)";
+        }
+    }
+}
+
+function newBoard()
+{
+    // pick a random board from the list
+    boardNumber = Math.floor(Math.random()*currentBoardList.length);
+    console.log("Board "+(boardNumber+1)+" of "+currentBoardList.length);
+    currentBoard = cloneBoard(currentBoardList[boardNumber]);
+    initialTiles = getFilledTiles(currentBoard);
+    message.innerHTML = ""; // reset message
+    drawBoard();
+}
+
+function restart()
+{
+    currentBoard = cloneBoard(currentBoardList[boardNumber]);
+    drawBoard();
+}
+
+window.onload = function()
+{
+    // set header
+    function setHeader()
+    {
+        kindle.messaging.sendMessage("com.lab126.chromebar", "configureChrome",
+            {
+                "appId": "com.katem26.demo",
+                "topNavBar":
+                {
+                    "template": "title",
+                    "title": "Binary Sudoku"
+                }
+            }
+        );
+    }
+
+    window.kindle.appmgr.ongo = function (ctx) {
+        setHeader();
+    }
+
+
+    boardDisplay = document.querySelector("#board");
+    message = document.querySelector("#message");
+    newBoard();
+}
+
+
+
+
+
+
